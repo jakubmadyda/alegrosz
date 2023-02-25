aecdc375be49e33c80f90dce4d9f3d4e61e39563
Jakub Madyda <jakub.madyda17@gmail.com>
2023-02-25 08:47:47 +0100

    list of products data

---
+++ src/components/Products.jsx
@@ -0,0 +1,51 @@
+import {useEffect, useState} from "react";
+
+const productList = [
+    {
+        "id": 1,
+        "name": "Pomidor czerwony",
+        "description": " ",
+        "price": 3.19,
+        "category": "warzywa",
+        "subcategories": "pomidory",
+        "image": "",
+        "comments": []
+    },
+    {
+        "id": 2,
+        "name": "Pomidor żółty",
+        "description": " ",
+        "price": 4.50,
+        "category": "warzywa",
+        "subcategories": "pomidory",
+        "image": "",
+        "comments": []
+    },
+    {
+        "id": 3,
+        "name": "Pomidor malinowy",
+        "description": " ",
+        "price": 5,
+        "category": "warzywa",
+        "subcategories": "pomidory",
+        "image": "",
+        "comments": []
+    }
+]
+
+function Products() {
+
+    const [listOfProducts, setListOfProducts] = useState([]);
+
+    useEffect(() => {
+        setListOfProducts(productList)
+    }, [])
+
+    return (
+        <div>
+            Products
+        </div>
+    );
+}
+
+export default Products;
